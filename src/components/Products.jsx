ba4abad832354116f4fa785d3ac70181c4812ed7
kkamzzu <kkamzzu@gmail.com>
2023-04-14 22:50:43 +0900

    End[useEffect2]

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
   // 우리가 원하는 함수를 여기 넣으면 됨
   // 콜백함수를 전달할것.
   useEffect(() => {
-    fetch("data/products.json")
+    // 체크가 변경 될때마다 useEffect이 전체적으로 다시호출됨.
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("뜨끈한 데이터 네트워크에서 받아옴");
@@ -16,10 +17,17 @@ export default function Products() {
     return () => {
       console.log("깨끗하게 청소하는 일들을 합니다");
     };
-  }, []);
+  }, [checked]);
   // 딱 한번만 데이터를 받고싶으면 텅빈 배열을 넣어줘.
   return (
     <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Show only sale</label>
       <ul>
         {products.map((products) => (
           <li key={products.id}>
@@ -30,7 +38,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
